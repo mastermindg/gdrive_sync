628013f148ec9bbbff205af2a31b47423f64ac1d
Ken Jenney <mastermindg@gmail.com>
2017-10-14 19:52:00 +0000

    Log Copying and Moving

--- gdrive_sync.rb
+++ gdrive_sync.rb
@@ -12,8 +12,10 @@ notifier = INotify::Notifier.new
 def uploadFile(file,foldersync,googlefolder)
   session = GoogleDrive::Session.from_config("config.json")
   filename = File.basename(file)
+  puts "\tCopying #{file}" 
   uploaded = session.upload_from_file("#{foldersync}/#{filename}", filename, convert: false)
   unless googlefolder.nil?
+    puts "\tMoving it to #{googlefolder}" 
     session.collection_by_title("Plex Cloud Sync").add(uploaded)
   end
   session.root_collection.remove(uploaded)
