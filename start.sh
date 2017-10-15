f8e3765c54cd927a882f0ab2de2150fccd011b55
Ken Jenney <kjenney@gmail.com>
2017-10-14 22:10:27 -0400

    Make initial setup easier to follow

--- start.sh
+++ start.sh
@@ -3,11 +3,13 @@
 # Check for config.json
 if [ -f config.json ]; then
 	echo "It exists"
+else
+	echo "config.json doesn't exist...We need this to get moving"
+	echo "Follow the README and create the config.json from the credentials page"
+	exit 1
 fi
 
-exit
-
-# Check architecture
+# Build - check architecture
 uname -a | grep -q armv
 if [ $? -eq 0 ]; then
 	echo "We're running on ARM"
@@ -17,7 +19,14 @@ else
 	docker build -f Dockerfile.x86 -t gdrive_sync .
 fi
 
-docker stop gdrive_sync
-docker rm gdrive_sync
-docker run -d --name gdrive_sync -v $PWD/files:/files gdrive_sync
-#docker exec -it gdrive_sync bash
+# Check if config.json has been updated by first run
+grep -q "refresh_token" config.json
+if [ $? -eq 0 ]; then
+	echo "You're config is all set!"
+	docker run -d --name gdrive_sync -v $PWD/files:/files gdrive_sync
+else
+	echo "You need to authenticate to get started..."
+	docker stop gdrive_sync && docker rm gdrive_sync
+	docker run -it --rm -v $PWD/config.json:/root/config.json gdrive_sync
+fi
+
