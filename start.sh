ef2126f2fa8daeb2294867a0463864db7df54931
Ken Jenney <kjenney@gmail.com>
2017-10-15 00:04:46 -0400

    Pick your folder

--- start.sh
+++ start.sh
@@ -30,8 +30,10 @@ function checkforfolder {
 }
 
 function startit {
-	echo "Let's start it!"
-	docker run -d --name gdrive_sync -v $PWD/files:/files gdrive_sync > /dev/null 2>&1
+	echo "Let's get ready to start it!"
+	echo "Which folder do you want to sync to Google Drive?: i.e. /myshare/files"
+	read myfolder
+	docker run -d --name gdrive_sync -v $myfolder:/files gdrive_sync > /dev/null 2>&1
 }
 
 function buildit {
